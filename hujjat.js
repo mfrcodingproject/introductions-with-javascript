a471a0f95411e23b19d27eadbfb19aa58761456a
HujjatGh <hujjat.pg@gmail.com>
2023-01-30 20:24:07 +0100

    my branch

---
+++ hujjat.js
@@ -0,0 +1,7 @@
+const hujjat = {
+    role: "student",
+    profession: "dentistry",
+    interests: "programming and web development",
+    hobbies: ["sports", "hiking"],
+    sisters: 3,
+    brothers: 2
